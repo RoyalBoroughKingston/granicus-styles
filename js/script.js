7c3fa76ae698c0e11213758a647752d4191ac9df
birol-ks <birol.goktepe@kingston.gov.uk>
2023-03-22 10:00:21 +0000

    dev branch prepared with /fs-filestore-eu/rbkingston/Branding/dev folder without dash

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-$('.char-counter').after("<div class='char-count'></div>");
+
+if (window.location.href.indexOf("fillform") > -1) {
+
+    $('<link href="https://fs-filestore-eu.s3-eu-west-1.amazonaws.com/rbkingston/Branding/dev/css/core.css" rel="stylesheet">').appendTo("head");
+	$('<link href="https://fs-filestore-eu.s3-eu-west-1.amazonaws.com/rbkingston/Branding/dev/css/form.css" rel="stylesheet">').appendTo("head");
+}
+
+
+
+
 
 // Include .char-counter class to input fields and set the char limit:important
+$('.char-counter').after("<div class='char-count'></div>");
+
+
 
 $('.char-counter').keyup(function () {
 var max = $(this).attr('maxlength');
