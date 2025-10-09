e4f569d69dad7b7e06c4dba6290cbe9ec67e9a7e
SidonieAbeKSCLOUD <sidonie.abe@kingston.gov.uk>
2025-10-09 15:09:50 +0100

    Dev to test (#36)
    * Update font size of custom label class
    
    * Change button order on mobile
    
    * Re order button alignment for desktop
    
    * Fix margin auto on fillinButtonsRight
    
    * Fix margin auto on fillinButtonsLeft
    
    * Add cancel button renaming
    
    * Hide Yes & Stop Warnings button
    
    * Adjusted table head padding

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
+// Include .char-counter class to input fields and set the char limit:important
 $('.char-counter').after("<div class='char-count'></div>");
 
-// Include .char-counter class to input fields and set the char limit:important
-
 $('.char-counter').keyup(function () {
-var max = $(this).attr('maxlength');
-var len = $(this).val().length;
-if (len >= max) {
-$(this).next().text(' you have reached the character limit');
-} else {
-var char = max - len;
-$(this).next().text(char + ' characters left of '+ max);
-}
+    var max = $(this).attr('maxlength');
+    var len = $(this).val().length;
+    if (len >= max) {
+        $(this).next().text(' you have reached the character limit');
+    } else {
+        var char = max - len;
+        $(this).next().text(char + ' characters left of ' + max);
+    }
 });
+
+// Change the cancel button name
+$(".cancelbutton").text("Leave form");
